d95ab93692975c6ea5293ba306d89e4320bebc33
Alexandre AUDA <Alexandre AUDA>
2016-11-12 01:50:41 +0100

    Problème2 et Problème3 du commit précédent fixés!

--- src/TowerDefense.cpp
+++ src/TowerDefense.cpp
@@ -152,15 +152,15 @@ La methode watchdog de la classe TowerDefense etablie la procedure d'attaque des
 @param: La methode watchdog prend un parametre obligatoire: un pointeur sur un vecteur de pointeur de type Monstre.
 -param1: Le pointeur sur un vecteur de pointeur de type Monstre qui contient la liste de tout les Monstres present sur la grille
 */
-void TowerDefense::watchdog(std::vector <Monstre *> *MonstreList){
-    float distanceTowerMonstre=0.0;
+void TowerDefense::watchdog(vector <Monstre *> *MonstreList){
+    float distanceTowerMonstre=10.0;
     float currentDistanceTowerMonstre=0.0;
     int index=0;
 
     //Si il n'y a pas de Monstre sur la grille
     if(MonstreList->size() == 0){
-        m_targetx=m_centerTowerx;
-        m_targety=m_centerTowery;
+        m_targetx=m_centerTowerx;//On place le target sur le centre de la tourelle
+        m_targety=m_centerTowery;//On place le target sur le centre de la tourelle
     }
     //Sinon
     else{
@@ -175,16 +175,17 @@ void TowerDefense::watchdog(std::vector <Monstre *> *MonstreList){
                 index=i;//On retient l'index ou est situe le Monstre le plus proche de la tourelle
             }
         }//Fin de la boucle for
-        //Si m_timer modulo m_cadence est egale a 0
-        if(m_timer%m_cadence == 0){
+        //Si m_timer modulo m_cadence est egale a 0 et que la distance entre la tourelle et le monstre est inferieur a m_portee*(distance entre le centre de deux blocks positionnes en diagonnal) -> Formule donnee par l'application du theoreme de Pythagore en fonction de la largeur et de la hauteur des blocks.
+        if(m_timer%m_cadence == 0 && currentDistanceTowerMonstre<=(m_portee*sqrt(m_blockBase.getm_width()*m_blockBase.getm_width()+m_blockBase.getm_height()*m_blockBase.getm_height()))){
             m_targetx=(*MonstreList)[index]->monsterCenterx();//On met a jour le target de la tourelle qui se place au centre du Monstre
             m_targety=(*MonstreList)[index]->monsterCentery();//On met a jour le target de la tourelle qui se place au centre du Monstre
             this->attaque((*MonstreList)[index]);//La tourelle attaque le Monstre le plus proche d'elle
             m_timer++;//On incremente le compteur
         }
+        //Sinon
         else{
-            m_targetx=m_centerTowerx;
-            m_targety=m_centerTowery;
+            m_targetx=m_centerTowerx;//On place le target sur le centre de la tourelle
+            m_targety=m_centerTowery;//On place le target sur le centre de la tourelle
             m_timer++;//On incremente le compteur
         }
     }
