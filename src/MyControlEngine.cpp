625bd2813d9a5f64a8c2f454c806fd284a9fcf4d
Alexandre AUDA <Alexandre AUDA>
2016-12-21 02:40:30 +0100

    Intégration des évolutions de tourelles dans MyControlEngine

--- src/MyControlEngine.cpp
+++ src/MyControlEngine.cpp
@@ -84,6 +84,14 @@ void MyControlEngine::MouseCallback(int button, int state, int x, int y){
             int const costTowerDefenseOrange(LibMatrix::loadIntInFile("CostTowerDefense/CostTowerDefenseOrange.txt"));//Cout d'une tourelle orange
             int const costTowerDefensePurple(LibMatrix::loadIntInFile("CostTowerDefense/CostTowerDefensePurple.txt"));//Cout d'une tourelle violette
             int const costTowerDefenseRicochet(LibMatrix::loadIntInFile("CostTowerDefense/CostTowerDefenseRicochet.txt"));//Cout d'une tourelle Ricochet
+            int const costTowerDefenseYellowLongShot(LibMatrix::loadIntInFile("CostTowerDefense/CostTowerDefenseYellowLongShot.txt"));//Cout d'une tourelle TowerDefenseYellowLongShot
+            int const costTowerDefenseOrangeLongShot(LibMatrix::loadIntInFile("CostTowerDefense/CostTowerDefenseOrangeLongShot.txt"));//Cout d'une tourelle TowerDefenseOrangeLongShot
+            int const costTowerDefensePurpleLongShot(LibMatrix::loadIntInFile("CostTowerDefense/CostTowerDefensePurpleLongShot.txt"));//Cout d'une tourelle TowerDefensePurpleLongShot
+            int const costTowerDefenseRicochetMediumLevel(LibMatrix::loadIntInFile("CostTowerDefense/CostTowerDefenseRicochetMediumLevel.txt"));//Cout d'une tourelle TowerDefenseRicochetMediumLevel
+            int const costTowerDefenseYellowSniper(LibMatrix::loadIntInFile("CostTowerDefense/CostTowerDefenseYellowSniper.txt"));//Cout d'une tourelle TowerDefenseYellowSniper
+            int const costTowerDefenseOrangeSniper(LibMatrix::loadIntInFile("CostTowerDefense/CostTowerDefenseOrangeSniper.txt"));//Cout d'une tourelle costTowerDefenseOrangeSniper
+            int const costTowerDefensePurpleSniper(LibMatrix::loadIntInFile("CostTowerDefense/CostTowerDefensePurpleSniper.txt"));//Cout d'une tourelle TowerDefensePurpleSniper
+            int const costTowerDefenseRicochetHighLevel(LibMatrix::loadIntInFile("CostTowerDefense/CostTowerDefenseRicochetHighLevel.txt"));//Cout d'une tourelle TowerDefenseRicochetHighLevel
 
             //on parcourt la grille
             for(int x=0;x<12;x++){
@@ -151,6 +159,118 @@ void MyControlEngine::MouseCallback(int button, int state, int x, int y){
                                     }
                                     break;
 
+                            case 5: //Si le player a assez d'argent pour se payer une tourelle TowerDefenseYellowLongShot
+                                    if(m_player->haveMoneyEnough(costTowerDefenseYellowLongShot) == 1){
+                                        m_player->spendMoney(costTowerDefenseYellowLongShot);//Le joueur paye le cout d'une tourelle TowerDefenseYellowLongShot
+                                        m_TowerDefenseList->push_back(new TowerDefenseYellowLongShot((m_grilleDeJeu[x][y])));//on rajoute une tourelle TowerDefenseYellowLongShot sur la case correspondante
+                                        (m_grilleDeJeu[x][y]).setm_isFreeID(1);//on met l'attribut m_isFreeID de la case correspondante a 1, ce qui veut dire que cette case est occupee par une tourelle
+                                        (m_grilleDeJeu[x][y]).setm_colorTowerID(5);//on met l'attribut m_colorTowerID de la case correspondante a 5, ce qui veut dire que cette case est occupee par une tourelle TowerDefenseYellowLongShot
+                                        cout<<"Vous avez deposer une tourelle TowerDefenseYellowLongShot!"<<endl<<endl;
+                                    }
+                                    //Si le player n'a pas assez d'argent pour se payer une tourelle TowerDefenseYellowLongShot
+                                    else{
+                                        cout<<"Vous n'avez pas assez d'argent pour deposer une tourelle TowerDefenseYellowLongShot!"<<endl<<endl;
+                                    }
+                                    break;
+
+                            case 6: //Si le player a assez d'argent pour se payer une tourelle TowerDefenseOrangeLongShot
+                                    if(m_player->haveMoneyEnough(costTowerDefenseOrangeLongShot) == 1){
+                                        m_player->spendMoney(costTowerDefenseOrangeLongShot);//Le joueur paye le cout d'une tourelle TowerDefenseOrangeLongShot
+                                        m_TowerDefenseList->push_back(new TowerDefenseOrangeLongShot((m_grilleDeJeu[x][y])));//on rajoute une tourelle TowerDefenseOrangeLongShot sur la case correspondante
+                                        (m_grilleDeJeu[x][y]).setm_isFreeID(1);//on met l'attribut m_isFreeID de la case correspondante a 1, ce qui veut dire que cette case est occupee par une tourelle
+                                        (m_grilleDeJeu[x][y]).setm_colorTowerID(6);//on met l'attribut m_colorTowerID de la case correspondante a 6, ce qui veut dire que cette case est occupee par une tourelle TowerDefenseOrangeLongShot
+                                        cout<<"Vous avez deposer une tourelle TowerDefenseOrangeLongShot!"<<endl<<endl;
+                                    }
+                                    //Si le player n'a pas assez d'argent pour se payer une tourelle TowerDefenseOrangeLongShot
+                                    else{
+                                        cout<<"Vous n'avez pas assez d'argent pour deposer une tourelle TowerDefenseOrangeLongShot!"<<endl<<endl;
+                                    }
+                                    break;
+
+                            case 7: //Si le player a assez d'argent pour se payer une tourelle TowerDefensePurpleLongShot
+                                    if(m_player->haveMoneyEnough(costTowerDefensePurpleLongShot) == 1){
+                                        m_player->spendMoney(costTowerDefensePurpleLongShot);//Le joueur paye le cout d'une tourelle TowerDefensePurpleLongShot
+                                        m_TowerDefenseList->push_back(new TowerDefensePurpleLongShot((m_grilleDeJeu[x][y])));//on rajoute une tourelle TowerDefensePurpleLongShot sur la case correspondante
+                                        (m_grilleDeJeu[x][y]).setm_isFreeID(1);//on met l'attribut m_isFreeID de la case correspondante a 1, ce qui veut dire que cette case est occupee par une tourelle
+                                        (m_grilleDeJeu[x][y]).setm_colorTowerID(7);//on met l'attribut m_colorTowerID de la case correspondante a 6, ce qui veut dire que cette case est occupee par une tourelle TowerDefensePurpleLongShot
+                                        cout<<"Vous avez deposer une tourelle TowerDefensePurpleLongShot!"<<endl<<endl;
+                                    }
+                                    //Si le player n'a pas assez d'argent pour se payer une tourelle TowerDefensePurpleLongShot
+                                    else{
+                                        cout<<"Vous n'avez pas assez d'argent pour deposer une tourelle TowerDefensePurpleLongShot!"<<endl<<endl;
+                                    }
+                                    break;
+
+                            case 8: //Si le player a assez d'argent pour se payer une tourelle TowerDefenseRicochetMediumLevel
+                                    if(m_player->haveMoneyEnough(costTowerDefenseRicochetMediumLevel) == 1){
+                                        m_player->spendMoney(costTowerDefenseRicochetMediumLevel);//Le joueur paye le cout d'une tourelle TowerDefenseRicochetMediumLevel
+                                        m_TowerDefenseList->push_back(new TowerDefenseRicochetMediumLevel((m_grilleDeJeu[x][y])));//on rajoute une tourelle TowerDefenseRicochetMediumLevel sur la case correspondante
+                                        (m_grilleDeJeu[x][y]).setm_isFreeID(1);//on met l'attribut m_isFreeID de la case correspondante a 1, ce qui veut dire que cette case est occupee par une tourelle
+                                        (m_grilleDeJeu[x][y]).setm_colorTowerID(8);//on met l'attribut m_colorTowerID de la case correspondante a 6, ce qui veut dire que cette case est occupee par une tourelle TowerDefenseRicochetMediumLevel
+                                        cout<<"Vous avez deposer une tourelle TowerDefenseRicochetMediumLevel!"<<endl<<endl;
+                                    }
+                                    //Si le player n'a pas assez d'argent pour se payer une tourelle TowerDefenseRicochetMediumLevel
+                                    else{
+                                        cout<<"Vous n'avez pas assez d'argent pour deposer une tourelle TowerDefenseRicochetMediumLevel!"<<endl<<endl;
+                                    }
+                                    break;
+
+                            case 9: //Si le player a assez d'argent pour se payer une tourelle TowerDefenseYellowSniper
+                                    if(m_player->haveMoneyEnough(costTowerDefenseYellowSniper) == 1){
+                                        m_player->spendMoney(costTowerDefenseYellowSniper);//Le joueur paye le cout d'une tourelle TowerDefenseYellowSniper
+                                        m_TowerDefenseList->push_back(new TowerDefenseYellowSniper((m_grilleDeJeu[x][y])));//on rajoute une tourelle TowerDefenseYellowSniper sur la case correspondante
+                                        (m_grilleDeJeu[x][y]).setm_isFreeID(1);//on met l'attribut m_isFreeID de la case correspondante a 1, ce qui veut dire que cette case est occupee par une tourelle
+                                        (m_grilleDeJeu[x][y]).setm_colorTowerID(9);//on met l'attribut m_colorTowerID de la case correspondante a 6, ce qui veut dire que cette case est occupee par une tourelle TowerDefenseYellowSniper
+                                        cout<<"Vous avez deposer une tourelle TowerDefenseYellowSniper!"<<endl<<endl;
+                                    }
+                                    //Si le player n'a pas assez d'argent pour se payer une tourelle TowerDefenseYellowSniper
+                                    else{
+                                        cout<<"Vous n'avez pas assez d'argent pour deposer une tourelle TowerDefenseYellowSniper!"<<endl<<endl;
+                                    }
+                                    break;
+
+                            case 10: //Si le player a assez d'argent pour se payer une tourelle TowerDefenseOrangeSniper
+                                    if(m_player->haveMoneyEnough(costTowerDefenseOrangeSniper) == 1){
+                                        m_player->spendMoney(costTowerDefenseOrangeSniper);//Le joueur paye le cout d'une tourelle TowerDefenseOrangeSniper
+                                        m_TowerDefenseList->push_back(new TowerDefenseOrangeSniper((m_grilleDeJeu[x][y])));//on rajoute une tourelle TowerDefenseOrangeSniper sur la case correspondante
+                                        (m_grilleDeJeu[x][y]).setm_isFreeID(1);//on met l'attribut m_isFreeID de la case correspondante a 1, ce qui veut dire que cette case est occupee par une tourelle
+                                        (m_grilleDeJeu[x][y]).setm_colorTowerID(10);//on met l'attribut m_colorTowerID de la case correspondante a 6, ce qui veut dire que cette case est occupee par une tourelle TowerDefenseOrangeSniper
+                                        cout<<"Vous avez deposer une tourelle TowerDefenseOrangeSniper!"<<endl<<endl;
+                                    }
+                                    //Si le player n'a pas assez d'argent pour se payer une tourelle TowerDefenseOrangeSniper
+                                    else{
+                                        cout<<"Vous n'avez pas assez d'argent pour deposer une tourelle TowerDefenseOrangeSniper!"<<endl<<endl;
+                                    }
+                                    break;
+
+                            case 11: //Si le player a assez d'argent pour se payer une tourelle TowerDefensePurpleSniper
+                                    if(m_player->haveMoneyEnough(costTowerDefensePurpleSniper) == 1){
+                                        m_player->spendMoney(costTowerDefensePurpleSniper);//Le joueur paye le cout d'une tourelle TowerDefensePurpleSniper
+                                        m_TowerDefenseList->push_back(new TowerDefensePurpleSniper((m_grilleDeJeu[x][y])));//on rajoute une tourelle TowerDefensePurpleSniper sur la case correspondante
+                                        (m_grilleDeJeu[x][y]).setm_isFreeID(1);//on met l'attribut m_isFreeID de la case correspondante a 1, ce qui veut dire que cette case est occupee par une tourelle
+                                        (m_grilleDeJeu[x][y]).setm_colorTowerID(11);//on met l'attribut m_colorTowerID de la case correspondante a 6, ce qui veut dire que cette case est occupee par une tourelle TowerDefensePurpleSniper
+                                        cout<<"Vous avez deposer une tourelle TowerDefensePurpleSniper!"<<endl<<endl;
+                                    }
+                                    //Si le player n'a pas assez d'argent pour se payer une tourelle TowerDefensePurpleSniper
+                                    else{
+                                        cout<<"Vous n'avez pas assez d'argent pour deposer une tourelle TowerDefensePurpleSniper!"<<endl<<endl;
+                                    }
+                                    break;
+
+                            case 12: //Si le player a assez d'argent pour se payer une tourelle TowerDefenseRicochetHighLevel
+                                    if(m_player->haveMoneyEnough(costTowerDefenseRicochetHighLevel) == 1){
+                                        m_player->spendMoney(costTowerDefenseRicochetHighLevel);//Le joueur paye le cout d'une tourelle TowerDefenseRicochetHighLevel
+                                        m_TowerDefenseList->push_back(new TowerDefenseRicochetHighLevel((m_grilleDeJeu[x][y])));//on rajoute une tourelle TowerDefenseRicochetHighLevel sur la case correspondante
+                                        (m_grilleDeJeu[x][y]).setm_isFreeID(1);//on met l'attribut m_isFreeID de la case correspondante a 1, ce qui veut dire que cette case est occupee par une tourelle
+                                        (m_grilleDeJeu[x][y]).setm_colorTowerID(12);//on met l'attribut m_colorTowerID de la case correspondante a 6, ce qui veut dire que cette case est occupee par une tourelle TowerDefenseRicochetHighLevel
+                                        cout<<"Vous avez deposer une tourelle TowerDefenseRicochetHighLevel!"<<endl<<endl;
+                                    }
+                                    //Si le player n'a pas assez d'argent pour se payer une tourelle TowerDefenseRicochetHighLevel
+                                    else{
+                                        cout<<"Vous n'avez pas assez d'argent pour deposer une tourelle TowerDefenseRicochetHighLevel!"<<endl<<endl;
+                                    }
+                                    break;
+
                             default: break;
                         }
                     }
@@ -207,6 +327,46 @@ void MyControlEngine::MouseCallback(int button, int state, int x, int y){
 
                             case 4: m_stockColorTower=4;//on met la couleur de la souris a 4 ce qui correspond a la couleur de la tourelle Ricochet
                                     cout<<"Vous avez choisi une tourelle Ricochet dans le Store:"<<endl;
+                                    cout<<"-Veuillez la placer sur la grille!"<<endl<<endl;
+                                    break;
+
+                            case 5: m_stockColorTower=5;//on met la couleur de la souris a 5 ce qui correspond a la couleur de la tourelle TowerDefenseYellowLongShot
+                                    cout<<"Vous avez choisi une tourelle TowerDefenseYellowLongShot dans le Store:"<<endl;
+                                    cout<<"-Veuillez la placer sur la grille!"<<endl<<endl;
+                                    break;
+
+                            case 6: m_stockColorTower=6;//on met la couleur de la souris a 6 ce qui correspond a la couleur de la tourelle TowerDefenseOrangeLongShot
+                                    cout<<"Vous avez choisi une tourelle TowerDefenseOrangeLongShot dans le Store:"<<endl;
+                                    cout<<"-Veuillez la placer sur la grille!"<<endl<<endl;
+                                    break;
+
+                            case 7: m_stockColorTower=7;//on met la couleur de la souris a 7 ce qui correspond a la couleur de la tourelle TowerDefensePurpleLongShot
+                                    cout<<"Vous avez choisi une tourelle TowerDefensePurpleLongShot dans le Store:"<<endl;
+                                    cout<<"-Veuillez la placer sur la grille!"<<endl<<endl;
+                                    break;
+
+                            case 8: m_stockColorTower=8;//on met la couleur de la souris a 8 ce qui correspond a la couleur de la tourelle TowerDefenseRicochetMediumLevel
+                                    cout<<"Vous avez choisi une tourelle TowerDefenseRicochetMediumLevel dans le Store:"<<endl;
+                                    cout<<"-Veuillez la placer sur la grille!"<<endl<<endl;
+                                    break;
+
+                            case 9: m_stockColorTower=9;//on met la couleur de la souris a 9 ce qui correspond a la couleur de la tourelle TowerDefenseYellowSniper
+                                    cout<<"Vous avez choisi une tourelle TowerDefenseYellowSniper dans le Store:"<<endl;
+                                    cout<<"-Veuillez la placer sur la grille!"<<endl<<endl;
+                                    break;
+
+                            case 10: m_stockColorTower=10;//on met la couleur de la souris a 10 ce qui correspond a la couleur de la tourelle TowerDefenseOrangeSniper
+                                    cout<<"Vous avez choisi une tourelle TowerDefenseOrangeSniper dans le Store:"<<endl;
+                                    cout<<"-Veuillez la placer sur la grille!"<<endl<<endl;
+                                    break;
+
+                            case 11: m_stockColorTower=11;//on met la couleur de la souris a 11 ce qui correspond a la couleur de la tourelle TowerDefensePurpleSniper
+                                    cout<<"Vous avez choisi une tourelle TowerDefensePurpleSniper dans le Store:"<<endl;
+                                    cout<<"-Veuillez la placer sur la grille!"<<endl<<endl;
+                                    break;
+
+                            case 12: m_stockColorTower=12;//on met la couleur de la souris a 12 ce qui correspond a la couleur de la tourelle TowerDefenseRicochetHighLevel
+                                    cout<<"Vous avez choisi une tourelle TowerDefenseRicochetHighLevel dans le Store:"<<endl;
                                     cout<<"-Veuillez la placer sur la grille!"<<endl<<endl;
                                     break;
 
